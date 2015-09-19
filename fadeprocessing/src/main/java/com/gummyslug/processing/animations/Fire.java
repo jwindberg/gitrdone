f97abd503cf4651046f4ec0c0aac82bb88dd2898
jwind <jwind@192.168.254.12>
2015-09-19 15:16:16 -0700

    longer, faster, harder

--- fadeprocessing/src/main/java/com/gummyslug/processing/animations/Fire.java
+++ fadeprocessing/src/main/java/com/gummyslug/processing/animations/Fire.java
@@ -12,37 +12,31 @@ public class Fire extends AnimationApplet {
 
 	float noiseScale = 100;
 	float nz = 0;
-	float line_lenght = 10;
+	float line_lenght = 15;
 	float drawCount = 30;
+	int flameX;
+	int flameY;
+	int step = 2;
 
 	public void setupInternal() {
-		
 		opc.showLocations(false);
-		// setup
 		background(0);
 	}
 
-	//////////////////////////////////////////////////////////////////
-
 	public void draw() {
-
-		// background (0);
-
 		lineDraw();
-
 		fadeToBG();
-
 	}
 
-	//////////////////////////////////////////////////////////////////
-	// FUNCTION
-
 	void lineDraw() {
+		flameX += step;
+		if (flameX > width) {
+			flameX = 0;
+		}
+		flameY = height - 2;
 
 		for (int i = 0; i < 5; i++) {
-			// lines.add ( new Curves( (width / 2 + random( 0 , 100)) - 100 ,
-			// (height / 2 + random( 0 , 50)) - 100));
-			lines.add(new Curves((mouseX + random(0, 70)) - 35, (mouseY + random(0, 50)) - 50));
+			lines.add(new Curves((flameX + random(0, 140)) - 35, (flameY + random(0, 50)) - 50));
 		}
 
 		noFill();
@@ -50,29 +44,22 @@ public class Fire extends AnimationApplet {
 		for (int i = 0; i < lines.size(); i++) {
 
 			Curves crv = lines.get(i);
-			// println(crv.x1);
 
 			float b = noise(crv.x4 / noiseScale, crv.y4 / noiseScale, nz) * -PI;
 
 			crv.addCoord(cos(b) * line_lenght, sin(b) * line_lenght);
 
 			strokeWeight(20 * (drawCount - crv.n) / drawCount + 1);
-			// stroke ( 240 + random(5,15) , 20 + random(0,50) , random(0,10),
-			// (drawCount - crv.n) / drawCount );
 			stroke(240 + random(5, 15), 20 + random(0, 50), random(0, 10), 50);
 
 			curve(crv.x1, crv.y1, crv.x2, crv.y2, crv.x3, crv.y3, crv.x4, crv.y4);
-			// curve ( 0 , 0 , 10 , 50 , 35 , 55 , 80 , 120);
 
 			if (crv.n >= drawCount) {
 				lines.remove(i);
 				i--;
-
 			}
-
 		}
 		nz += 0.01;
-
 	}
 
 	void fadeToBG() {
@@ -82,14 +69,9 @@ public class Fire extends AnimationApplet {
 
 	}
 
-	//////////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////
-
-	//
 	class Curves {
 		float x1, x2, x3, x4;
 		float y1, y2, y3, y4;
-
 		int n = 0;
 
 		Curves(float tmpX, float tmpY) {
@@ -103,7 +85,6 @@ public class Fire extends AnimationApplet {
 			x2 = x3;
 			x3 = x4;
 			x4 += tmpdX;
-
 			y1 = y2;
 			y2 = y3;
 			y3 = y4;
